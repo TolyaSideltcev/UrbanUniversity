48ce530a62f9e97dff824b5d06d19503dd1eb91e
TolyaSideltcev <tolya05092000@mail.ru>
2025-02-06 15:52:36 +0700

    HomeWork3

---
+++ variables.py
@@ -0,0 +1,5 @@
+homeworks = 12
+hours = 1.5
+name = 'Python'
+time =  hours / homeworks
+print('Курс:', name, ', всего задач:', homeworks, ', затрачено часов:', hours, ', среднее время выполнения', time, ' часа.')
